3df0e32daff302c54cbfe15cea4e19d23d1f6871
Marcos Paulo <marcos.pereira@rethink.devo>
2023-03-09 15:11:12 -0300

    Desafio Class adicionado

---
+++ typescript/desafio produto/index.ts
@@ -0,0 +1,10 @@
+class Produto {
+  constructor(
+    public nome: string,
+    public preco: number,
+    public desconto: number = 0
+  ) {}
+}
+
+const produto_1 = new Produto("Arroz", 12)
+const produto_2 = new Produto("Feijão", 10, 0.2)
